b23dc9c52988483c6b0245f88c14b95bf6a54959
bruneau <bruneauvoisine.florian@gmail.com>
2013-01-15 17:53:35 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '77f790bcf96eecb0222c11c9ad93ae1fc82350aecd5129f4e1bfe77c49d86ac2cf15770496df3b98529d27b50fdbd6493fd74fef03865c6ef15552bb50d62b91'
